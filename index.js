9c8bf15eccbe92c85ccc0494d1768b9354a7c2e3
Gerbert Pesquera <gerbert.pesquera@gmail.com>
2022-02-02 12:34:19 -0500

    commit tree bbbede22e2ab55551b16f6c0591fb9b899ac6613

--- index.js
+++ index.js
@@ -1,11 +1,26 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(tripInFeet){
+  if(tripInFeet <= 400){
+    return "This one is on me!" 
+  } else if(tripInFeet > 2500){
+    return 'No can do.'
+  } else if(tripInFeet > 2000){
+    return 'I will gladly take your thirty bucks.'
+  } 
+};
+
+function ternaryCheckCity(city){
+  return (city === 'NYC'? 'Ok, sounds good.': 'No go.');
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
-
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+  switch (tip){
+    case 'generous':
+      return 'Thank you so much.';
+      break;
+    case 'not as generous':
+      return 'Thank you.';
+      break;
+    case 'thanks for everything':
+      return 'Bye.'
+  }
 }
